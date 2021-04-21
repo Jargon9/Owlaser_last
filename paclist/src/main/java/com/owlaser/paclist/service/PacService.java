e4f2c554a9c440ca5a1ccf8a4e2ba83a12431d74
Jargon96 <jargon9@pku.edu.cn>
2021-04-21 17:14:00 +0800

    Update pacService

--- paclist/src/main/java/com/owlaser/paclist/service/PacService.java
+++ paclist/src/main/java/com/owlaser/paclist/service/PacService.java
@@ -76,7 +76,7 @@ public class PacService {
                 String SqlArtifactId = pacDao.getArtifactId(dependency.getArtifact_id(), dependency.getGroup_id());
                 if(SqlArtifactId == null){
                     String url = "https://mvnrepository.com/artifact/" + dependency.getGroup_id() + "/" + dependency.getArtifact_id();
-                    http2GetService.getAll(url, dependency);
+                    getAll(url, dependency);
                     dependenciesList.add(dependency);
                     pacDao.insert(dependency);
                 }
@@ -103,81 +103,7 @@ public class PacService {
      *在所用url中得到依赖包的所有参数（版本号，热度等）
      */
     public void getAll(String url, Dependency dependency){
-        String tmp="";
-        ArrayList<String> versionList= new ArrayList<>();
-        ArrayList<Integer> usageList = new ArrayList<>();
-        ArrayList<String> licenseList = new ArrayList<>();
-        try {
-
-            Connection conn = Jsoup.connect(url).timeout(5000);
-//            conn.header("cookie","_ga=GA1.2.1305858049.1572185055; __gads=Test; __cfduid=d4ed591bc6767fb8c03529ea441b656891618574502; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; _gid=GA1.2.1088741524.1618814695; MVN_SESSION=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7InVpZCI6ImE4NjgxYzkxLWY4YzItMTFlOS04YWU2LTZkMDdlMDAzMmQ1NCJ9LCJleHAiOjE2NTAzNTcxODcsIm5iZiI6MTYxODgyMTE4NywiaWF0IjoxNjE4ODIxMTg3fQ.dKuKkC_ugmVX7uaEa8UwIMQa1E9FlByPCLswc4HjodU; _gat=1; __cf_bm=1f05273e4a67f4c9f13708e1f058ae73844a49ca-1618821188-1800-AYFCIDTAJ3wLh9dLRqziPdUiDPcxOnGzDyciwMfr3FXEnENosmQdN32p/nQLvR0H8Xk28wcVCD5ekLXjjcogIicI/sip7EoAQrVws4IOV7TBBnnnGkQKYpOFqKvb/LP8rQ==") ;
-            conn.header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-            conn.header("accept-encoding", "gzip, deflate, br");
-            conn.header("accept-language", "zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6");
-            conn.header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36");
-//            conn.header("cache-control", "max-age=0");
-//            conn.header("sec-fetch-dest", "document");
-//            conn.header("sec-fetch-mode", "navigate");
-//            conn.header("sec-fetch-site", "none");
-//            conn.header("sec-fetch-user", "?1");
-//            conn.header("upgrade-insecure-requests", "1");
-            org.jsoup.nodes.Document document = conn.get();
-            Elements latestversionValue = document.getElementsByTag("td");
-            Elements versionnameValue = document.select(".vbtn");
-            Elements usageValue = document.getElementsByTag("td");
-            Elements licenseValue = document.select(".b").select(".lic");
-
-            //找到得到新版本号
-            for(org.jsoup.nodes.Element element : latestversionValue) {
-                System.out.println(element);
-                if (element.text().matches("^\\d{1,3}\\..*$") || element.text().matches("^\\d{1,3}$")) {
-                    tmp = element.text();
-                    String ragex = "[^(a-zA-Z)]";
-                    String stableSign = tmp.replaceAll(ragex, "");////提取版本号中的字母部分，以查看是否是稳定版本
-                    if (stableSign.equals("Final")|| stableSign.equals("RELEASE") || stableSign.equals("") ) {
-                        dependency.setStable_version(tmp);
-                        break;
-                    }
-                }
-            }
-
-            //得到版本号数组
-            for(org.jsoup.nodes.Element element:versionnameValue){
-                org.jsoup.nodes.Document elementdoc = Jsoup.parse(element.toString());
-                Elements versionName = elementdoc.select("a");
-                versionList.add(versionName.text());
-
-            }
-
-            //得到热度数组
-            for(org.jsoup.nodes.Element element : usageValue){
-                if(element.text().matches("\\d{1,3}(,\\d{3})*$")){ //取使用量，对于三位分割法去掉中间的逗号
-                    String rawString = element.text();
-                    String removeStr = ",";
-                    rawString = rawString.replace(removeStr,"");
-                    usageList.add(Integer.parseInt(rawString));
-                }
-            }
-
-            //得到license数组
-            for(org.jsoup.nodes.Element element:licenseValue){
-                org.jsoup.nodes.Document elementdoc = Jsoup.parse(element.toString());
-                Elements license = elementdoc.select("span");
-                licenseList.add(license.text());
-            }
-
-
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
-        dependency.setVersionList(versionList);
-        dependency.setUsageList(usageList);
-        String license = String.join("  ",licenseList);
-        // dependency.setLicenseList(licenseList);
-        dependency.setLicense(license);
-        String bestVersion = versionList.get(usageList.indexOf(Collections.max(usageList)));
-        dependency.setPopular_version(bestVersion);
+        http2GetService.getAll(url, dependency);
     }
 
     /**
